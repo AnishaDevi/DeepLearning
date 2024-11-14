60794aedcd2be95d45d0b0fce599cc0181189e93
Anisha Devi <96512703+AnishaDevi@users.noreply.github.com>
2024-11-14 21:12:48 +0530

    Added VectorDemo file

---
+++ VectorizationDemo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+import numpy as np
+import time
+
+a = np.random.rand(10000000)
+b = np.random.rand(10000000)
+
+#Vectorized Version
+tic = time.time()
+c = np.dot(a,b)
+toc = time.time()
+print(c)
+print("Vectorized version time taken:",str(1000*(toc-tic))+"ms\n")
+
+#Non Vectorized Version
+c = 0
+tic = time.time()
+for i in range(10000000):
+    c += a[i] * b[i]
+toc = time.time()
+print(c)
+print("Non Vectorized version time taken:",str(1000*(toc-tic))+"ms")
+
+    
+
